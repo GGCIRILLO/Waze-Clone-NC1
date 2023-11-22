bc09a303337414ba7c252cfddc3cf8e928b7d602
Luigi Cirillo <luigicirillo23@gmail.com>
2023-11-22 17:50:59 +0100

    zoom camera and highlight just the selected item

--- NC1/ViewModel/MapViewModel.swift
+++ NC1/ViewModel/MapViewModel.swift
@@ -18,33 +18,38 @@ class MapViewModel: ObservableObject {
     @Published var routeDisplaying = false
     @Published var showDetails: Bool = false
     @Published var cameraPosition: MapCameraPosition = .automatic
-
-
+    
+    
     func fetchRoute() {
-            if let selected = selected {
-                let request = MKDirections.Request()
-                request.source = MKMapItem(placemark: .init(coordinate: CLLocationCoordinate2D(latitude: locationManager.region.center.latitude, longitude: locationManager.region.center.longitude)))
-                request.destination = selected
-                
-                Task {
-                    do {
-                        let result = try await MKDirections(request: request).calculate()
-
-                        // Perform the following updates on the main thread
-                        Task {
-                            self.route = result.routes.first
-                            self.routeDestination = selected
-                            
+        if let selected = selected {
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: .init(coordinate: CLLocationCoordinate2D(latitude: locationManager.region.center.latitude, longitude: locationManager.region.center.longitude)))
+            request.destination = selected
+            
+            Task {
+                do {
+                    let result = try await MKDirections(request: request).calculate()
+                    
+                    // Perform the following updates on the main thread
+                    Task {
+                        self.route = result.routes.first
+                        self.routeDestination = selected
+                        self.routeDisplaying = true
+                        
+                        
+                        withAnimation(.snappy) {
                             if let rect = self.route?.polyline.boundingMapRect, self.routeDisplaying {
                                 self.cameraPosition = .rect(rect)
                             }
-                        }.cancel()
-
-                    } catch {
-                        // Handle errors if needed
-                        print("Error calculating route: \(error.localizedDescription)")
-                    }
+                            
+                        }
+                    }.cancel()
+                    
+                } catch {
+                    // Handle errors if needed
+                    print("Error calculating route: \(error.localizedDescription)")
                 }
             }
         }
+    }
 }
